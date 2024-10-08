c84e4e938e7376ec928d8575098e2614eb6df7e0
Sven Schroeder <schuen@gmx.de>
2024-10-08 09:51:23 +0200

    first commit

---
+++ src/main/java/com/svenschroeder/jte_templates_ex/JteTemplatesExApplication.java
@@ -0,0 +1,13 @@
+package com.svenschroeder.jte_templates_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JteTemplatesExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JteTemplatesExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svenschroeder/jte_templates_ex/JteTemplatesExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svenschroeder.jte_templates_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JteTemplatesExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
