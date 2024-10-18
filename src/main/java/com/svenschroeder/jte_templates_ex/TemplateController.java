3350a133daf925167366395e23c4a320484244cd
Sven Schroeder <schuen@gmx.de>
2024-10-18 18:10:34 +0200

    add two more pages

--- src/main/java/com/svenschroeder/jte_templates_ex/TemplateController.java
+++ src/main/java/com/svenschroeder/jte_templates_ex/TemplateController.java
@@ -1,15 +1,32 @@
 package com.svenschroeder.jte_templates_ex;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TemplateController {
-    
+
     @GetMapping("/")
-    public String home(Model model){
-        model.addAttribute("username", "hans");
+    public String home(Model model) {
+        model.addAttribute("username", "Sven");
         return "pages/home";
+    }
+
+    @GetMapping("/team")
+    public String team(Model model){
+        List<String> members = List.of("Alice", "Hans", "Peter", "Dhewie");
+        model.addAttribute("teamMembers", members); 
+        return "pages/team";
+    }
+
+    @GetMapping("projects")
+    public String projects(Model model){
+        List<String> projects = List.of("P1", "P2", "P3");
+        model.addAttribute("username", "Twirney");
+        model.addAttribute("projects", projects);
+        return "pages/projects";
     }
 }
